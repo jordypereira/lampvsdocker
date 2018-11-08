e78c1c544c316cf88590c083724663d097ecfcdd
Jordy Pereira <jordy.pereira@hotmail.com>
2018-11-08 13:38:23 +0100

    Setup Vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: 'LAMP vs Docker',
+  description:
+    "For school I had to do a comparative study. We'll see how fast we can start a LAMP Server using LAMP or Docker on an Ubuntu server.",
+  themeConfig: {
+    lastUpdated: 'Last Updated',
+    repo: 'perjor/learn_testing',
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'My Website', link: 'https://jordypereira.be/' },
+    ],
+    sidebar: ['/'],
+  },
+}
